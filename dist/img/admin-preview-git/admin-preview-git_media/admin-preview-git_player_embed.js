5c4dbf3d1cf9c70e5bda71b98a0c44f6a316d0fb
YounesDG <62108466+YounesDG@users.noreply.github.com>
2020-07-13 16:27:22 +0100

    preview-project
    image to preview the project

---
+++ dist/img/admin-preview-git/admin-preview-git_media/admin-preview-git_player_embed.js
@@ -0,0 +1,2 @@
+var oeTags = '<img src="admin-preview-git_media/admin-preview-git.gif" width="1348" height="668" alt=""/>';         
+document.write( oeTags );
---
+++ dist/img/admin-preview/admin-preview_media/admin-preview_player_embed.js
@@ -0,0 +1,2 @@
+var oeTags = '<img src="admin-preview_media/admin-preview.gif" width="1348" height="668" alt=""/>';         
+document.write( oeTags );
